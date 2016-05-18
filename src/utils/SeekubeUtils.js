a992ddd0781abfbdc2218b6a1a9fe5081ed5070b
Jonathan Forget <jonathan.forget@epitech.eu>
2016-05-18 11:42:47 +0200

    Add SeekubeUtils

---
+++ src/utils/SeekubeUtils.js
@@ -0,0 +1,41 @@
+const SEEKUBE_LOCAL_STORAGE_KEY = 'seekube:clientId';
+
+var SeekubeUtils = {
+
+    _generateGUID: function() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+    },
+
+    getOrCreateGUID: function() {
+        var guid;
+
+        if (localStorage) {
+            guid = localStorage.getItem(SEEKUBE_LOCAL_STORAGE_KEY);
+        }
+        if (!guid) {
+            guid = this.generateGuid();
+            localStorage.setItem(SEEKUBE_LOCAL_STORAGE_KEY, this.guid);
+        }
+        return guid;
+    },
+
+    payloadToUrlEncoded: function(payload) {
+        var urlEncodedData = "";
+        var urlEncodedDataPairs = [];
+        var name;
+
+        for(name in payload) {
+            urlEncodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(payload[name]));
+        }
+
+        urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
+
+        return urlEncodedData;
+    }
+};
